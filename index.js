64c1e603b796b94e06edf39f40d0b22ec08ccd73
Pedro Delgado <pedro.delgado2017@mesacharter.org>
2021-11-15 15:21:40 +0000

    code challenge

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 //write your code here
+function findLongestWord (string){
+    let longest;
+    let maxLength = 0;
+    for(let i = 0; i < string.length; i++){
+        if(string[i].length > maxLength){
+            maxLength = string[i].length
+            longest = string[i]
+        }
+    }
+    return longest
+}
+console.log(findLongestWord(["bird", "sax", "door", "igloo"]))
